9c41db5cf7185fc854792e8ec84e83d7e6c5e898
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-23 23:10:04 -0300

    Imported Checkbox from Material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -4,10 +4,22 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatToolbarModule, MatExpansionModule, MatCardModule],
-  exports: [MatToolbarModule, MatExpansionModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatCheckboxModule,
+  ],
+  exports: [
+    MatToolbarModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatCheckboxModule,
+  ],
 })
 export class MaterialModule {}
