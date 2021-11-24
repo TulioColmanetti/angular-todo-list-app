a61b45901a8a781d402b5dd48c9670cd10fb83e8
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-23 22:01:20 -0300

    Re-ordered imports and small refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
 
-import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
+
 import { MaterialModule } from '../material/material.module';
+
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
