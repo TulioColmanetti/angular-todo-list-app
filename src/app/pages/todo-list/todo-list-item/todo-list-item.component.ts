dfbc7fd49c6bb5b6f4d0d9866c9a8d758807542e
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-23 22:39:53 -0300

    Use of directives to show multiple components and pass data through property databinding

--- src/app/pages/todo-list/todo-list-item/todo-list-item.component.ts
+++ src/app/pages/todo-list/todo-list-item/todo-list-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Task } from './../../../models/task.model';
 
 @Component({
   selector: 'app-todo-list-item',
   templateUrl: './todo-list-item.component.html',
-  styleUrls: ['./todo-list-item.component.css']
+  styleUrls: ['./todo-list-item.component.css'],
 })
 export class TodoListItemComponent implements OnInit {
+  @Input('taskObj') task?: Task;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
