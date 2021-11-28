ef7a20e086b15adbd50dcedfed77744fb664dba8
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-28 19:25:28 -0300

    Refactored to add databinding for edit/delete

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { map, Observable } from 'rxjs';
 import { Task } from 'src/app/models/task.model';
 import { TodoListService } from 'src/app/services/todo-list.service';
 
@@ -13,18 +14,52 @@ export class TodoListComponent implements OnInit {
   // taskList: Task[] = [];
   taskList$?: Observable<Task[]>;
 
-  constructor(private todoListService: TodoListService) {}
+  constructor(
+    private todoListService: TodoListService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.taskList$ = this.todoListService.getTasks();
+    // this.taskList$ = this.todoListService.getTasks();
+    this.getTaskList();
     // this.todoListService.getTasks().subscribe((taskList: Task[]) => {
     //   this.taskList = taskList;
     // });
+  }
+
+  getTaskList() {
+    this.taskList$ = this.todoListService.getTasks();
   }
 
   markAsDone(obj: { id: number; value: boolean }) {
     // const id = obj.id;
     // const value = obj.value;
     // this.taskList$[id].done = value;
+
+    const sub = this.todoListService
+      .updateTask(obj.id, { done: obj.value })
+      .subscribe(() => {
+        sub.unsubscribe();
+      });
+  }
+
+  editTask(id: number) {
+    const sub = this.todoListService
+      .updateTask(id, { done: true })
+      .subscribe(() => {
+        this.router.navigate(['/update-task']);
+        sub.unsubscribe();
+      });
+  }
+
+  deleteTask(id: number) {
+    const sub = this.todoListService.deleteTask(id).subscribe(() => {
+      this.getTaskList();
+      sub.unsubscribe();
+    });
+
+    // this.taskList$ = this.taskList$?.pipe(
+    //   map((items) => items.filter((item) => (!item.id ? item.id !== id : true)))
+    // );
   }
 }
