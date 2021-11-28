0e5e70cf4618b7bcc8fb33d07df6cede275cdc4a
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-28 19:23:37 -0300

    Created template/component for creating task

--- src/app/pages/create-task/create-task.component.ts
+++ src/app/pages/create-task/create-task.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { TaskPriority } from 'src/app/models/task.model';
+import { TodoListService } from 'src/app/services/todo-list.service';
 
 @Component({
   selector: 'app-create-task',
   templateUrl: './create-task.component.html',
-  styleUrls: ['./create-task.component.css']
+  styleUrls: ['./create-task.component.css'],
 })
 export class CreateTaskComponent implements OnInit {
+  taskForm?: FormGroup;
+  taskPriorities = [TaskPriority.Low, TaskPriority.Medium, TaskPriority.High];
 
-  constructor() { }
+  constructor(
+    private todoListService: TodoListService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.taskForm = new FormGroup({
+      title: new FormControl(null),
+      description: new FormControl(null),
+      dueDate: new FormControl(null),
+      priority: new FormControl(null),
+      done: new FormControl(null),
+    });
   }
 
+  onSubmit() {
+    const sub = this.todoListService
+      .createTask({
+        title: this.taskForm?.controls['title'].value,
+        description: this.taskForm?.controls['description'].value,
+        dueDate: this.taskForm?.controls['dueDate'].value,
+        priority: this.taskForm?.controls['priority'].value,
+        labels: [],
+        done: this.taskForm?.controls['done'].value,
+      })
+      .subscribe(() => {
+        this.router.navigate(['']);
+        sub.unsubscribe();
+      });
+  }
 }
