f6b85094ca9e030fbd5a0da4523d7ef13849f9ad
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-28 19:19:49 -0300

    Changed route name for Create functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CreateTaskComponent } from './pages/create-task/create-task.component';
+import { UpdateTaskComponent } from './pages/update-task/update-task.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', canActivate: [AuthGuard], component: TodoListComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'task', canActivate: [AuthGuard], component: CreateTaskComponent },
+  {
+    path: 'create-task',
+    canActivate: [AuthGuard],
+    component: CreateTaskComponent,
+  },
+  {
+    path: 'update-task',
+    canActivate: [AuthGuard],
+    component: UpdateTaskComponent,
+  },
   { path: '**', redirectTo: '' },
 ];
 
