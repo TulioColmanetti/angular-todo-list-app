00216b76d595d1a63601d618e5b7726bd3664d69
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-11-27 23:16:23 -0300

    Created 2 new components and their routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateTaskComponent } from './pages/create-task/create-task.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodoListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'task', component: CreateTaskComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,11 @@ import { MaterialModule } from '../material/material.module';
 
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListItemComponent } from './todo-list/todo-list-item/todo-list-item.component';
+import { LoginComponent } from './login/login.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
 
 @NgModule({
-  declarations: [TodoListComponent, TodoListItemComponent],
+  declarations: [TodoListComponent, TodoListItemComponent, LoginComponent, CreateTaskComponent],
   imports: [CommonModule, MaterialModule],
   exports: [TodoListComponent],
 })
